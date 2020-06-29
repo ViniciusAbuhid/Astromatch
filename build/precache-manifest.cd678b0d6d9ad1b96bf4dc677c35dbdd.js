3a23296baee897fd1f87f59edf0a493d15be94a1
Vinicius <viniciusabuhid@gmail.com>
2020-06-29 18:18:44 -0300

    cloning project

---
+++ build/precache-manifest.cd678b0d6d9ad1b96bf4dc677c35dbdd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed13960353ac39a5ddea752812228499",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d672a9c75a9639a0d753",
+    "url": "/static/js/2.5b234805.chunk.js"
+  },
+  {
+    "revision": "14afe5197955785b3383",
+    "url": "/static/js/main.7e5ed976.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
